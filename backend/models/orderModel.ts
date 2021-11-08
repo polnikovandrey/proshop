fd2ab7c182cf874ec901c499052c416bd7280a54
Andrey Polnikov <polnikov.andrey@gmail.com>
2021-11-08 21:19:38 +0300

    + mongodb models for user, product and order ...1

---
+++ backend/models/orderModel.ts
@@ -0,0 +1,141 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { User, userSchema } from "./userModel";
+import { Product, productSchema } from "./productModel";
+
+export interface Order {
+    user: User,
+    orderItems: OrderItem[],
+    shippingAddress: Object,        // TODO !!!
+    paymentMethod: string,
+    paymentResult: Object,          // TODO !!!
+    taxPrice: number,
+    shippingPrice: number,
+    totalPrice: number,
+    paid: boolean,
+    paidAt: Date,
+    delivered: boolean,
+    deliveredAt: Date,
+    email: string,
+    password: string,
+    admin: boolean
+}
+
+export interface OrderItem {
+    name: string,
+    quality: number,
+    image: string,
+    price: number,
+    product: Product
+}
+
+const orderItemSchema: Schema<OrderItem> = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        quality: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: productSchema,
+            required: true,
+            ref: 'Product'
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const orderSchema: Schema<Order> = new mongoose.Schema(
+    {
+        user: {
+            type: userSchema,
+            required: true,
+            ref: 'User'
+        },
+        orderItems: [ orderItemSchema ],
+        shippingAddress: {
+            address: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            postalCode: {
+                type: String,
+                required: true
+            },
+            country: {
+                type: String,
+                required: true
+            }
+        },
+        paymentMethod: {
+            type: String,
+            required: true
+        },
+        paymentResult: {
+            id: {
+                type: String
+            },
+            status: {
+                type: String
+            },
+            updateTime: {
+                type: String
+            },
+            email_address: {
+                type: String
+            }
+        },
+        taxPrice: {
+            type: Number,
+            required: true,
+            default: 0.0
+        },
+        shippingPrice: {
+            type: Number,
+            required: true,
+            default: 0.0
+        },
+        totalPrice: {
+            type: Number,
+            required: true,
+            default: 0.0
+        },
+        paid: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        paidAt: {
+            type: Date
+        },
+        delivered: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        deliveredAt: {
+            type: Date
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const OrderModel: Model<Order> = mongoose.model<Order>('Order', orderSchema);
---
+++ backend/models/userModel.ts
@@ -0,0 +1,36 @@
+import mongoose, { Model, Schema } from "mongoose";
+
+export interface User {
+    name: string,
+    email: string,
+    password: string,
+    admin: boolean
+}
+
+export const userSchema: Schema<User> = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        admin: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const UserModel: Model<User> = mongoose.model<User>('User', userSchema);
