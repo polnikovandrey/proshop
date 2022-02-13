b0e3cea7bf29443b1b297c5a37d3ff913cb34af0
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-02-13 14:12:54 +0300

    + backend getUserById, updateUser endpoints

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -123,3 +123,39 @@ export const deleteUser = expressAsyncHandler(async (req: Request, res: Response
         throw new Error('User not found')
     }
 });
+
+// @desc    Get user by id
+// @route   GET /api/users/:id
+// @access  Private/Admin
+export const getUserById = expressAsyncHandler(async (req: Request, res: Response) => {
+    const user: UserDocument = await UserModel.findById(req.params.id).select('-password');
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+export const updateUser = expressAsyncHandler(async (req: Request, res: Response) => {
+    const aUser: UserDocument = await UserModel.findById(req.params.id);
+    if (aUser) {
+        aUser.name = req.body.name || aUser.name;
+        aUser.email = req.body.email || aUser.email;
+        aUser.admin = req.body.admin;
+        const updatedUser: UserDocument = await aUser.save();
+        res.json(
+            {
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                admin: updatedUser.admin
+            });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { authUser, deleteUser, getUserProfile, getUsers, registerUser, updateUserProfile } from "../controllers/userController.js";
+import { authUser, deleteUser, getUserById, getUserProfile, getUsers, registerUser, updateUser, updateUserProfile } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
 const userRouter: Router = express.Router();
@@ -7,6 +7,6 @@ const userRouter: Router = express.Router();
 userRouter.route('/').post(registerUser).get(protect, admin, getUsers);
 userRouter.post('/login', authUser);
 userRouter.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-userRouter.route('/:id').delete(protect, admin, deleteUser);
+userRouter.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
 
 export default userRouter;
