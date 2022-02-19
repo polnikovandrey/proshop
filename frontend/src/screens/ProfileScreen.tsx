9fde82572365c0aa28b205565b0ddfcfc3337d56
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-02-19 10:57:56 +0300

    fix Profile screen form group margins

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -61,19 +61,19 @@ const ProfileScreen = ({ history }: { history: History }) => {
                 { userProfileState.success && <Message variant='success'>Profile updated</Message>}
                 { userProfileState?.loading && <Loader/> }
                 <Form onSubmit={submitHandler}>
-                    <Form.Group controlId='name'>
+                    <Form.Group controlId='name' className='mb-3'>
                         <Form.Label>Name</Form.Label>
                         <Form.Control type='name' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group controlId='email'>
+                    <Form.Group controlId='email' className='mb-3'>
                         <Form.Label>Email Address</Form.Label>
                         <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group controlId='password'>
+                    <Form.Group controlId='password' className='mb-3'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group controlId='confirmPassword'>
+                    <Form.Group controlId='confirmPassword' className='mb-4'>
                         <Form.Label>Confirm Password</Form.Label>
                         <Form.Control type='password' placeholder='Confirm password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                     </Form.Group>
